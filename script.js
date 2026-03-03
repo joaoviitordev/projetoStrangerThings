087a50913d9b84f4389c02094a5fb58717329616
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2026-03-03 20:05:29 -0300

    efeito nos cards

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText)
+
     // Scroll Suave + Efeito Parallax com Gsap!
-ScrollSmoother.create ({
-    smooth: 1.5,
-    effects: true
-});
+    ScrollSmoother.create ({
+        smooth: 1.5,
+        effects: true
+    });
 
-/* EFEITOS DA HERO */
+    /* EFEITOS DA HERO */
     
-// Efeito de fade
-gsap.from(".hero", {
-    opacity: 0,
-    duration: 1.7
-});
+    // Efeito de fade
+    gsap.from(".hero", {
+        opacity: 0,
+        duration: 1.7
+    });
 
-// Efeito do Monstro vindo de cima
-gsap.from("picture:nth-child(1)",{
-    y: -70,
-    duration: 1.7
-});
+    // Efeito do Monstro vindo de cima
+    gsap.from("picture:nth-child(1)",{
+        y: -70,
+        duration: 1.7
+    });
 
-// Efeito dos personagens vindo de baixo
-gsap.from("picture:nth-child(2)",{
-    y: 70,
-    duration: 1.7
-});
+    // Efeito dos personagens vindo de baixo
+    gsap.from("picture:nth-child(2)",{
+        y: 70,
+        duration: 1.7
+    });
 
-/* EFEITO DOS CARDS */
+    /* EFEITO DOS CARDS */
     
-gsap.from(".card", {
-    opacity: 0,
-    duration: 1,
-    y: 30,
-    filter: "blur(10px)",
-    stagger: .3,
-});
-
+    gsap.from(".card", {
+        opacity: 0,
+        duration: 1,
+        y: 10,
+        filter: "blur(5px)",
+        stagger: .3,
+        scrollTrigger: {
+            markers: true
+        }
+    });
 });
